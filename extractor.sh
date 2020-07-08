5137980c559d002a0292f71cc52ec438e16fabe3
Georgi Naumov <gonaumov@gmail.com>
2020-07-08 16:36:34 +0300

    A fix

--- extractor.sh
+++ extractor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-total=`egrep --include="*.txt" -rwoih '[^[:space:]@]+@[^@[:space:]]+\.[^[:space:]]{2,4}' . | wc -l`
+total=`egrep --include="*.txt" -rwoih '[^[:space:]@]+@[^@[:space:]]+\.[^[:space:]]{2,4}' . | sort | uniq | wc -l`
 
 test -f results.json && rm results.json; egrep --include="*.txt" -rwoih '[^[:space:]@]+@[^@[:space:]]+\.[^[:space:]]{2,4}' . | sort | uniq | awk -v total=$total -f json-creator.awk
